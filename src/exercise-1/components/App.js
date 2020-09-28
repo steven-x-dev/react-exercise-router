de16ff09170db894f7f3ecf34b58c734a8e9f60c
steven-x-dev <steven-dev@qq.com>
2020-09-29 01:52:50 +0800

    [steven] feat: finished Home

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import Home from './Home';
+// import AboutUs from './MyProfile';
+// import MyProfile from './AboutUs';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Router />
+      <div className='app'>
+        <BrowserRouter>
+          <div className='nav-bar-wrapper'>
+            <div className='nav-bar'>
+              <NavLink to='/'>Home</NavLink>
+              <NavLink to='/my-profile'>My Profile</NavLink>
+              <NavLink to='/about-us'>About Us</NavLink>
+            </div>
+          </div>
+          <main>
+            <div className='content-wrapper'>
+              <Switch>
+                <Route exact path='/'>
+                  <Home />
+                </Route>
+                {/*<Route path='/my-profile' component={MyProfile} />*/}
+                {/*<Route path='/about-us' component={AboutUs} />*/}
+              </Switch>
+            </div>
+          </main>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/exercise-1/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+  render() {
+    return (
+      <div>
+        This is a beautiful Home Page.
+        <br />
+        And the url is '/'.
+      </div>
+    );
+  }
+}
+
+export default Home;
